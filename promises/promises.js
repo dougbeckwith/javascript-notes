c8ec7b2873a629a1bb704375b19d693b34c9db8b
Doug Beckwith <dougcbeckwith@gmail.com>
2022-11-06 09:07:04 -0800

    update promises notes

--- promises/promises.js
+++ promises/promises.js
@@ -1,30 +1,17 @@
-// Async JavaScript
+// PROMISES
 
-//Governs how we pperform tasks which take some time to complete
-//e.g Getting data from a database
-//Start something now and finish it later
-
-//Synchronous Javascript
-//Javascript can run ONE statement at a time
-//It is a single threaded language
-
-//HTTP Requests
-//Make HTTP requests to get data from another server
-//We make these requests to the API endpoints
-
-// The point of promises is to handle async things
+// The point of promises is to handle ASYNC things
 // Promises can be in three states
 // 1. Pending
 // 2. Fulfilled
 // 3. Rejected
 
-// the built in function fetch returns a promise.
+// The built in function fetch returns a promise.
 // let promise = fetch(url)
 
-//.then()
+// Using .then().catch() to handle the promise
 //.then() takes in a function that runs if the promise is fufilled
 //.catch() takes in a function that runs if the promise is rejected
-
 function getDataThenCatch() {
   fetch('https://catfact.ninja/fact')
     .then((response) => {
@@ -35,8 +22,8 @@ function getDataThenCatch() {
     .catch((error) => console.log(error))
 }
 
-//Using async await
-async function getData() {
+//Using async await to handle the promise
+async function getDataAsyncAwait() {
   try {
     const response = await fetch('https://catfact.ninja/fact')
     const data = await response.json()
@@ -47,4 +34,4 @@ async function getData() {
 }
 
 getDataThenCatch()
-getData()
+getDataAsyncAwait()
